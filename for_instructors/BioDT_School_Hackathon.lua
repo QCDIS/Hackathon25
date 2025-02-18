3f9d20125d9a1cc61e70d154c1ccbc45533b0124
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-02-18 17:10:22 +0200

    Set PYTHONUSERBASE

--- for_instructors/BioDT_School_Hackathon.lua
+++ for_instructors/BioDT_School_Hackathon.lua
@@ -1,8 +1,14 @@
 -- Jupyter
 -- depends_on("module1", "module2")
-prepend_path("PATH", "/projappl/project_465001732/BioDT_School_Hackathon_env/bin")
+local project = "project_465001732"
+local envdir = pathJoin("/projappl", project, "BioDT_School_Hackathon_env")
+local workdir = pathJoin("/scratch", project, os.getenv("USER"))
+local basename = "BioDT"
 
-setenv("_COURSE_BASE_NAME", "BioDT")
+prepend_path("PATH", pathJoin(envdir, "bin"))
+setenv("PYTHONUSERBASE", pathJoin(workdir, basename, "env", "Python"))
+
+setenv("_COURSE_BASE_NAME", basename)
 setenv("_COURSE_NOTEBOOK", "Hackathon25/README.ipynb")
 setenv("_COURSE_GIT_REPO", "https://github.com/BioDT/Hackathon25.git")
 setenv("_COURSE_GIT_REF", "")
