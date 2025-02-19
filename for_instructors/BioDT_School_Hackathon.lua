72b2e7f422fa1291159cc071aa92e9f3702509e2
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-02-19 16:40:16 +0200

    Use automatically the latest environment

--- for_instructors/BioDT_School_Hackathon.lua
+++ for_instructors/BioDT_School_Hackathon.lua
@@ -1,16 +1,18 @@
 -- Jupyter
 -- depends_on("module1", "module2")
 local project = "project_465001732"
-local envdir = pathJoin("/projappl", project, "BioDT_School_Hackathon_env")
+local projdir = pathJoin("/projappl", project)
 local workdir = pathJoin("/scratch", project, os.getenv("USER"))
 local basename = "BioDT"
 
-prepend_path("PATH", pathJoin(envdir, "bin"))
 setenv("PYTHONUSERBASE", pathJoin(workdir, basename, "env", "Python"))
 setenv("R_LIBS_USER", pathJoin(workdir, basename, "env", "R"))
 
 execute {cmd="mkdir -p \"$PYTHONUSERBASE\"", modeA={"load"}}
 execute {cmd="mkdir -p \"$R_LIBS_USER\"", modeA={"load"}}
+
+-- Search for the latest env and add it to the path
+execute {cmd="export PATH=$(ls -d " .. projdir .. "/BioDT_School_Hackathon_env_v* | tail -n 1)/bin:$PATH; echo PATH=$PATH", modeA={"load"}}
 
 setenv("_COURSE_BASE_NAME", basename)
 setenv("_COURSE_NOTEBOOK", "Hackathon25/README.ipynb")
