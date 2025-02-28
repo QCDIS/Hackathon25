f14b5f6abe318bbf0ebb9cf2055d53160105da40
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-02-28 19:34:17 +0200

    Fix hq installation

--- for_instructors/BioDT_School_Hackathon_post.sh
+++ for_instructors/BioDT_School_Hackathon_post.sh
@@ -5,11 +5,16 @@ if [ -z "$CW_INSTALLATION_PREFIX" ]; then
     exit 1
 fi
 
-BINDIR="$CW_INSTALLATION_PREFIX/bin"
-mkdir -p "$BINDIR"
+BIN1DIR="$CW_INSTALLATION_PREFIX/bin"
+BIN2DIR="$CW_INSTALLATION_PREFIX/_bin"
+mkdir -p "$BIN1DIR"
+mkdir -p "$BIN2DIR"
 
 wget https://github.com/It4innovations/hyperqueue/releases/download/v0.21.1/hq-v0.21.1-linux-x64.tar.gz
 tar -xvf hq-v0.21.1-linux-x64.tar.gz
+cp -v hq "$BIN1DIR/"
+cp -v hq "$BIN2DIR/"
+rm hq
 rm hq-v0.21.1-linux-x64.tar.gz
-mv hq "$BINDIR/"
+echo "DONE"
 
